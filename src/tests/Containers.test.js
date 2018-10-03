fe81f560bb12f53251f4d3357cd691b983c47b8e
Michal Turek <michael.turek@rizk.com>
2018-10-03 12:02:45 +0200

    test-mapStateToProps

--- src/tests/Containers.test.js
+++ src/tests/Containers.test.js
@@ -3,15 +3,45 @@ import { shallow } from "enzyme";
 import { Provider } from "react-redux";
 import store from "../store";
 import Main from "../containers/Main";
+// Create the mock store
+import configureMockStore from "redux-mock-store";
+const mockStore = configureMockStore();
 
 describe("Main Component", () => {
+  const wrapper = shallow(<Main store={store} />);
+
   it("should render without throwing an error", () => {
-    expect(
-      shallow(
-        <Provider store={store}>
-          <Main />
-        </Provider>
-      ).exists()
-    ).toBe(true);
+    expect(wrapper.exists()).toBe(true);
   });
+
+  beforeEach(() => {
+    const initialState = {
+      modal: {
+        isOpen: false
+      },
+      commentsList: {
+        isShown: true
+      },
+      comments: [],
+      profileInfo: {}
+    };
+    const store = mockStore(initialState);
+  });
+
+  it("should has property isOpen = false", () => {
+    expect(wrapper.props().modal).toHaveProperty("isOpen", false);
+  });
+
+  it("should has property isShown = true", () => {
+    expect(wrapper.props().commentsList).toHaveProperty("isShown", true);
+  });
+
+  it("should be array and has more than 0 items", () => {
+    expect(wrapper.props().comments.comments.length).toBeGreaterThan(0)
+  });
+
+  it("should be an object", () => {
+    expect(typeof wrapper.props().profileInfo).toBe('object')
+  });
+
 });
