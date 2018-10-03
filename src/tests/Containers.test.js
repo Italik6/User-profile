b4e1909d893a6154e414fab5ed9da3b1fd793104
Michal Turek <michael.turek@rizk.com>
2018-10-03 13:28:29 +0200

    test-toggle-comment-click

--- src/tests/Containers.test.js
+++ src/tests/Containers.test.js
@@ -3,12 +3,14 @@ import { shallow } from "enzyme";
 import { Provider } from "react-redux";
 import store from "../store";
 import Main from "../containers/Main";
+import { mapDispatchToProps } from "../containers/Main";
 // Create the mock store
 import configureMockStore from "redux-mock-store";
 const mockStore = configureMockStore();
 
 describe("Main Component", () => {
-  const wrapper = shallow(<Main store={store} />);
+  const toggleComment = jest.fn();
+  const wrapper = shallow(<Main store={store} toggleComment={toggleComment} />);
 
   it("should render without throwing an error", () => {
     expect(wrapper.exists()).toBe(true);
@@ -37,11 +39,14 @@ describe("Main Component", () => {
   });
 
   it("should be array and has more than 0 items", () => {
-    expect(wrapper.props().comments.comments.length).toBeGreaterThan(0)
+    expect(wrapper.props().comments.comments.length).toBeGreaterThan(0);
   });
 
   it("should be an object", () => {
-    expect(typeof wrapper.props().profileInfo).toBe('object')
+    expect(typeof wrapper.props().profileInfo).toBe("object");
   });
 
+  it("should be not call", () => {
+    expect(toggleComment).not.toBeCalled();
+  });
 });
